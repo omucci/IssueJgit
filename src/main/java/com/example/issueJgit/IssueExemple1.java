e106a4c2deb516d68d9647e7d8b1c0bf8fa66ef8
Orlando <o.mucci@reply.it>
2026-01-30 15:46:27 +0100

    test 3

--- src/main/java/com/example/issueJgit/IssueExemple1.java
+++ src/main/java/com/example/issueJgit/IssueExemple1.java
@@ -20,21 +20,28 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class IssueExemple1 implements FieldSetMapper<TestModel> {
 
-    @Override
-    public TestModel mapFieldSet(FieldSet fieldSet) {
-    	TestModel testModel = new TestModel();
+	@Override
+	public TestModel mapFieldSet(FieldSet fieldSet) {
+		TestModel testModel = new TestModel();
 
-        testModel.setCategory(fieldSet.readString(AppConstants.CATEGORY_CODE).trim());
-        testModel.setStatus("0");
-        testModel.setRegion(fieldSet.readString(AppConstants.REGION_CODE).trim());
-        testModel.setType(fieldSet.readString(AppConstants.TYPE_CODE).trim());
+		String category = fieldSet.readString(AppConstants.CATEGORY_CODE).trim();
+		String region = fieldSet.readString(AppConstants.REGION_CODE).trim();
+		String type = fieldSet.readString(AppConstants.TYPE_CODE).trim();
 
-        testModel.setFullName(fieldSet.readString(AppConstants.CATEGORY_NAME) +
-                " - " +
-                fieldSet.readString(AppConstants.TYPE_NAME) +
-                " - " +
-                fieldSet.readString(AppConstants.REGION_NAME));
+		if (!category.isEmpty() || !region.isEmpty() || !type.isEmpty()) {
 
-        return testModel;
-    }
+			testModel.setCategory(category);
+			testModel.setStatus("0");
+			testModel.setRegion(region);
+			testModel.setType(type);
+
+                        testModel.setFullName(fieldSet.readString(AppConstants.CATEGORY_NAME) +
+					" - " +
+					fieldSet.readString(AppConstants.TYPE_NAME) +
+					" - " +
+					fieldSet.readString(AppConstants.REGION_NAME));
+		}
+
+		return testModel;
+	}
 }
